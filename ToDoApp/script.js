100bce01d20490df1c9eba31e85540970cdaadbb
Martin Schneider <schnmart86@gmail.com>
2023-12-01 00:05:58 +0100

    feat: ToDoApp Checkboxes

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -3,10 +3,10 @@ const btn_add = document.getElementById("btn_add");
 const list = document.getElementById("list");
 
 let toDoArr = {
-  1: "eins",
-  2: "zwei",
-  3: "drei",
-  4: "vier",
+  0: "default",
+  1: "zwei",
+  2: "drei",
+  3: "vier",
 };
 let toDoArrSize = Object.getOwnPropertyNames(toDoArr).length;
 input.value = "TestValue";
@@ -16,15 +16,18 @@ input.value = "TestValue";
 // list.appendChild(addli);
 
 //!!!!!!!!!!ADD checkboxes!!!!!!!!!
+
 function writeList() {
   for (const key in toDoArr) {
-    list
-      .appendChild(document.createElement("li"))
-      .appendChild(document.createTextNode(toDoArr[key]));
+    const new_checkbox = document.createElement("input");
+    new_checkbox.type = "checkbox";
+    new_checkbox.id = key;
+    new_checkbox.style.marginRight = "2rem";
 
-    console.log(toDoArr);
-    console.log(toDoArr[key]);
-    console.log(input.value);
+    console.log(key);
+    list.appendChild(document.createElement("li"));
+    list.appendChild(new_checkbox);
+    list.appendChild(document.createTextNode(toDoArr[key]));
   }
 }
 
@@ -34,8 +37,22 @@ function render() {
 }
 
 btn_add.addEventListener("click", () => {
-  toDoArr[toDoArrSize + 1] = input.value;
+  if (input.value == "") {
+    return console.warn("NO INPUT");
+  }
+
+  let todo = "";
+  todo = input.value;
+  //cut Whitespaces off
+  while (todo[todo.length - 1] == " ") {
+    todo = todo.slice(0, todo.length - 1);
+  }
+  input.value = todo;
+
+  //put new todo to List
+  toDoArr[toDoArrSize] = todo;
   toDoArrSize += 1;
+
   render();
 });
 
